271d962b8c00d50e54b835d1182330cafaa8e9f9
Tales Patric <taleshy1@gmail.com>
2023-06-20 23:45:30 -0300

    removed the gh-page changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Projeto-Pokedex",
   server: {
     watch: {
       usePolling: true,
     },
   },
-})
+});
